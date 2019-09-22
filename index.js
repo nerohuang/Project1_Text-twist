086252da8d16cc93c0a01a64c693a6bf09fde096
NEROKING-DT\nerok <huangxingyu00@gmail.com>
2019-09-22 17:37:02 -0400

    test

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ var getLetters = function(){
 
 var showLetters = function(rack_get){
     let letters = document.getElementById("showletter");
-    console.log(rack_get);
+    console.log(rack_get[0]);
+    console.log(rack_get[1]);
+    console.log(rack_get.rack);
     letters.innerText = rack_get;
     //get_words(rack_get);
 }
