5cba0d5dd8af8409e1ca1a0af127d15eb221ddcb
NEROKING-DT\nerok <huangxingyu00@gmail.com>
2019-09-22 17:59:57 -0400

    words api add; update js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var start = function(){
     getLetters();
 }
 
-var getLetters = function(){
+var get_letters = function(){
     var rack_request = new XMLHttpRequest();
     rack_request.onload = function(){
         if (this.status == 200){
@@ -15,14 +15,22 @@ var getLetters = function(){
     rack_request.send();
 }
 
-var showLetters = function(rack_get){
-    let letters = document.getElementById("showletter");
-    letters.innerText = rack_get.rack;
-    //get_words(rack_get);
+var show_letters = function(rack_get){
+    document.getElementById("showletter").innerText = rack_get.rack;
+    get_words(rack_get.rack);
 }
 
-var get_words = function(letter_in){
-
+var get_words = function(rack){
+  var words_request = new XMLHttpRequest();
+  words_request.onload = function(){
+      if (this.status == 200){
+        console.log(this.response);
+        //showLetters(JSON.parse(this.response));
+      }
+      else console.log("No response");
+  };
+  words_request.open("GET", "words.php/${rack}");
+  words_request.send();
 }
 
 
---
+++ words.php
@@ -0,0 +1,44 @@
+<?php
+    //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
+    $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
+    if (!$dbhandle) die ($error);
+
+    $request = $_SERVER['REQUEST_METHOD'];
+    $uri = $_SERVER['PATH_INFO'];
+    $routes = explode("/", $uri);
+
+    $rack = $routes[1];
+
+    //this is a sample query which gets some data, the order by part shuffles the results
+    //the limit 0, 10 takes the first 10 results.
+    // you might want to consider taking more results, implementing "pagination",
+    // ordering by rank, etc.
+    for ($i = 0; $i < strlen($rank)-1; $i++){
+      for ($j = 1; $j <= (strlen($rank)-$i); $j++){
+        $choose_letter = substr($rank,$i,$j);
+        $query = "SELECT rack FROM racks WHERE rack='$choose_letter'";
+        $statement = $dbhandle->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $words = array_merge($words,$results);
+      }
+    }
+
+    //this next line could actually be used to provide user_given input to the query to
+    //avoid SQL injection attacks
+
+
+    //The results of the query are typically many rows of data
+    //there are several ways of getting the data out, iterating row by row,
+    //I chose to get associative arrays inside of a big array
+    //this will naturally create a pleasant array of JSON data when I echo in a couple lines
+
+
+    //this part is perhaps overkill but I wanted to set the HTTP headers and status code
+    //making to this line means everything was great with this request
+    header('HTTP/1.1 200 OK');
+    //this lets the browser know to expect json
+    header('Content-Type: application/json');
+    //this creates json and gives it back to the browser
+    echo json_encode(current($words));
+?>
