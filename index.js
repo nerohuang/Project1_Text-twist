2955cd4c8fd988f9e00433801c93ff1a08db445e
NEROKING-DT\nerok <huangxingyu00@gmail.com>
2019-09-22 17:20:20 -0400

    js api test

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-var showLetters = function(payload){
-    let letters = document.getElementById("showletter");
-    letters.innerText = payload;
-    //rackWords(payload);
-
-}
-
-var getLetters = function(){
-
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-        if (this.status == 200){
-          console.log(this.response);
-          showLetters(JSON.parse(this.response));
-        }
-        else console.log("No response");
-    };
-    xhr.open("GET", "rack.php/rack");
-    xhr.send();
-}
-
 var start = function(){
     getLetters();
 }
 
+var getLetters = function(){
+    var rack_request = new XMLHttpRequest();
+    rack_request.onload = function(){
+        if (this.status == 200){
+          //console.log(this.response);
+          showLetters(JSON.parse(this.response));
+        }
+        else console.log("No response");
+    };
+    rack_request.open("GET", "rack.php");
+    rack_request.send();
+}
+
+var showLetters = function(rack_get){
+    let letters = document.getElementById("showletter");
+    letters.innerText = rack_get;
+    //get_words(rack_get);
+}
+
+var get_words = function(letter_in){
+
+}
+
+
+
+
 start();
 
 document.getElementById("begin").addEventListener('click', function(){
-
     getLetters();
 });
--- rack.php
+++ rack.php
@@ -8,7 +8,7 @@
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination",
     // ordering by rank, etc.
-    $query = "SELECT rack FROM racks WHERE length = 7 and weight <= 7 order by random() limit 0, 1";
+    $query = "SELECT rack FROM racks WHERE length >3 and length < 7 and weight <= 7 order by random() limit 0, 1";
 
     //this next line could actually be used to provide user_given input to the query to
     //avoid SQL injection attacks
