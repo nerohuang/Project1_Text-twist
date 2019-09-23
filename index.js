7d67930e38dee112aea189bef94067ea354911f4
NEROKING-DT\nerok <huangxingyu00@gmail.com>
2019-09-23 03:26:59 -0400

    update

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ var split_words = [];
 var words_length = [];
 var words_length_num=[];
 
-var get_letters = function(){
-    var rack_request = new XMLHttpRequest();
-    rack_request.onload = function(){
-        if (this.status == 200){
-          //console.log(this.response);
-          show_letters(JSON.parse(this.response));
-        }
-        else console.log("No response");
-    };
-    rack_request.open("GET", "rack.php");
-    rack_request.send();
-}
-
-var show_letters = function(rack_get){
-    document.getElementById("showletter").innerText = rack_get.rack;
-    get_words(rack_get.rack);
-}
 
 var get_words = function(rack){
   var words_request = new XMLHttpRequest();
@@ -67,6 +50,7 @@ var count_words_length = function(words){
     }
   //  console.log(words_length);
   //  console.log(words_length_num);
+  display_words(words_length, words_length_num);
 }
 
 var display_words = function(words_length, words_length_num){
@@ -76,7 +60,11 @@ var display_words = function(words_length, words_length_num){
     document.getElementById("word_display").appendChild(note);
 }
 
+var start = function(){
+    getLetters();
+}
 
+start();
 
 
 document.getElementById("begin").addEventListener('click', function(){
