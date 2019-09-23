8746798efd12512ac4691c8e61a8a53fadd01906
NEROKING-DT\nerok <huangxingyu00@gmail.com>
2019-09-23 04:56:02 -0400

    update

--- index.js
+++ index.js
@@ -106,5 +106,8 @@ document.getElementById("submit_word").addEventListener('click', function(){
   console.log(words_length);
   console.log(words_length_num);
   var user_guess=document.getElementById("guessing_word").value;
+  if (words_list.indexOf(user_guess) != -1){
+    console.log('great');
+  }
   console.log(user_guess);
 });
