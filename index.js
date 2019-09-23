a9ae984bb0a84a28d56b97f239639f9586369b8e
NEROKING-DT\nerok <huangxingyu00@gmail.com>
2019-09-23 03:16:10 -0400

    no message

--- index.js
+++ index.js
@@ -85,8 +85,8 @@ document.getElementById("begin").addEventListener('click', function(){
     console.log(words_length);
     console.log(words_length_num);
 
-    var note = document.createElement("LI"); 
+    var note = document.createElement("LI");
     var textnode = document.createTextNode(words_length[0]);
     note.appendChild(textnode);
-    document.getElementById("word_display").appendChild(node);
+    document.getElementById("word_display").appendChild(note);
 });
